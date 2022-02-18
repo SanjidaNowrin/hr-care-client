9fc0d447e3603444726156547fe99b250b51f379
md-mh <mmahedi49@gmail.com>
2022-02-18 09:54:47 +0600

    add Employees at admin

---
+++ src/Components/Admin/Employees/Employee/Employee.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import SettingsIcon from '@mui/icons-material/Settings';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import Menu from '@mui/material/Menu';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+const StyledMenu = styled((props) => (
+    <Menu
+        elevation={0}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+        }}
+        {...props}
+    />
+))(({ theme }) => ({
+    '& .MuiPaper-root': {
+        borderRadius: 6,
+        marginTop: theme.spacing(1),
+        color:
+            theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+        boxShadow:
+            'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+        '& .MuiMenu-list': {
+            padding: '4px 0',
+        },
+        '& .MuiMenuItem-root': {
+            '& .MuiSvgIcon-root': {
+                fontSize: 18,
+                color: theme.palette.text.secondary,
+                marginRight: theme.spacing(1.5),
+            },
+            '&:active': {
+                backgroundColor: alpha(
+                    theme.palette.primary.main,
+                    theme.palette.action.selectedOpacity,
+                ),
+            },
+        },
+    },
+}));
+
+// export {StyledTableCell};
+const Employee = ({ item }) => {
+    const { name, phone, department, designation } = item;
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <>
+            <StyledTableRow>
+                <StyledTableCell component="th" scope="row">
+                    {name}
+                </StyledTableCell>
+                <StyledTableCell align="left">{phone}</StyledTableCell>
+                <StyledTableCell align="left">{department}</StyledTableCell>
+                <StyledTableCell align="left">{designation}</StyledTableCell>
+                <StyledTableCell align="right">
+                    <SettingsIcon
+                        id="demo-customized-button"
+                        aria-controls={open ? 'demo-customized-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        variant="contained"
+                        disableElevation
+                        sx={{ cursor: 'pointer' }}
+                        onClick={handleClick}
+                    />
+                </StyledTableCell>
+            </StyledTableRow>
+
+            <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'demo-customized-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose} disableRipple>
+                    <EditIcon />
+                    Edit
+                </MenuItem>
+                <MenuItem onClick={handleClose} disableRipple>
+                    <DeleteOutlineOutlinedIcon />
+                    Dalete
+                </MenuItem>
+            </StyledMenu>
+        </>
+    );
+};
+
+export default Employee;
