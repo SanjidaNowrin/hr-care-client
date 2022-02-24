78fa60e27f96754de2e33ee84115bd590a21aa8d
kawsaralidev <mdkawsarali346@gmail.com>
2022-02-24 16:57:21 +0600

    update information

--- src/Components/Employee/MyInfo/MyInfo.js
+++ src/Components/Employee/MyInfo/MyInfo.js
@@ -1,234 +1,282 @@
 import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  FormControl as FormGroup,
-  MenuItem,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Container, FormControl as FormGroup, MenuItem, TextField, Typography, useTheme } from "@mui/material";
 import useAuth from "../../../hooks/useAuth";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import Swal from "sweetalert2";
+import { makeStyles } from "@mui/styles";
 
 const MyInfo = () => {
-  const { user } = useAuth();
-  const { register, handleSubmit, reset } = useForm();
-  const onSubmit = (data) => {
-    axios.post("https://ancient-thicket-61342.herokuapp.com/employees/", data);
-    reset();
-    Swal.fire({
-      position: "middle",
-      icon: "success",
-      title: "Employee Information Sent Successfully",
-      showConfirmButton: false,
-      timer: 2000,
+    const { user } = useAuth();
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = (data) => {
+        axios.post("http://localhost:5000/employees", data);
+        reset();
+        Swal.fire({
+            position: "middle",
+            icon: "success",
+            title: "Employee Information Sent Successfully",
+            showConfirmButton: false,
+            timer: 2000,
+        });
+    };
+    const departments = [
+        {
+            value: "Human Resource",
+            label: "Human Resource",
+        },
+        {
+            value: "Information Technology",
+            label: "Information Technology",
+        },
+        {
+            value: "Marketing",
+            label: "Marketing",
+        },
+        {
+            value: "Accounting",
+            label: "Accounting",
+        },
+    ];
+
+    const [department, setDepartment] = useState();
+
+    const handleChange = (event) => {
+        setDepartment(event.target.value);
+    };
+
+    // useStyle
+
+    const theme = useTheme();
+    const useStyle = makeStyles({
+        dFlex: {
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "10px",
+            [theme.breakpoints.down("sx")]: {
+                display: "block !important",
+            },
+        },
+        middleField: {
+            marginLeft: "10px !important",
+            marginRight: "10px !important",
+        },
     });
-  };
-  const departments = [
-    {
-      value: "Human Resource",
-      label: "Human Resource",
-    },
-    {
-      value: "Information Technology",
-      label: "Information Technology",
-    },
-    {
-      value: "Marketing",
-      label: "Marketing",
-    },
-    {
-      value: "Accounting",
-      label: "Accounting",
-    },
-  ];
+    const { dFlex, middleField } = useStyle();
 
-  const [department, setDepartment] = useState();
+    return (
+        <Container>
+            <Typography
+                style={{
+                    textAlign: "center",
+                    fontWeight: "500",
+                    marginTop: "2rem",
+                    marginBottom: "2rem",
+                }}
+                variant="h4"
+            >
+                Fill Your <span style={{ color: " #01578A" }}>Information</span>
+            </Typography>
+            <Box>
+                {/* useForm */}
+                <FormGroup sx={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
+                    <Box className={dFlex}>
+                        <TextField
+                            {...register("name")}
+                            sx={{ width: "100% !important" }}
+                            id="outlined-basic"
+                            label="Name"
+                            type="text"
+                            variant="outlined"
+                            value={user?.displayName}
+                            required
+                        />
+                        <TextField
+                            className={middleField}
+                            sx={{ width: "100%" }}
+                            {...register("father")}
+                            id="outlined-basic"
+                            label="Father's Name"
+                            type="text"
+                            variant="outlined"
+                            required
+                        />
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("mother")}
+                            id="outlined-basic"
+                            label="Mother's Name"
+                            type="text"
+                            variant="outlined"
+                            required
+                        />
+                    </Box>
 
-  const handleChange = (event) => {
-    setDepartment(event.target.value);
-  };
+                    <Box className={dFlex}>
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("email")}
+                            id="outlined-basic"
+                            label="Email"
+                            type="email"
+                            variant="outlined"
+                            value={user?.email}
+                            required
+                        />
+                        <TextField
+                            className={middleField}
+                            sx={{ width: "100%" }}
+                            {...register("phone")}
+                            id="outlined-basic"
+                            label="Cell Number"
+                            type="number"
+                            variant="outlined"
+                            required
+                        />
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("nid")}
+                            id="outlined-basic"
+                            label="NID"
+                            type="number"
+                            variant="outlined"
+                            required
+                        />
+                    </Box>
 
-  return (
-    <>
-      <Typography
-        style={{
-          textAlign: "center",
-          fontWeight: "500",
-          marginTop: "2rem",
-          marginBottom: "2rem",
-        }}
-        variant="h4"
-      >
-        Fill Your <span style={{ color: " #01578A" }}>Information</span>
-      </Typography>
-      <FormGroup onSubmit={handleSubmit(onSubmit)}>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "30%" },
-          }}
-        >
-          <TextField
-            {...register("name")}
-            id="outlined-basic"
-            label="Name"
-            type="text"
-            variant="outlined"
-            value={user?.displayName}
-            required
-          />
-          <TextField
-            {...register("father")}
-            id="outlined-basic"
-            label="Father's Name"
-            type="text"
-            variant="outlined"
-            required
-          />
-          <TextField
-            {...register("mother")}
-            id="outlined-basic"
-            label="Mother's Name"
-            type="text"
-            variant="outlined"
-            required
-          />
-          <TextField
-            {...register("email")}
-            id="outlined-basic"
-            label="Email"
-            type="email"
-            variant="outlined"
-            value={user?.email}
-            required
-          />
-          <TextField
-            {...register("phone")}
-            id="outlined-basic"
-            label="Cell Number"
-            type="number"
-            variant="outlined"
-            required
-          />
-          <TextField
-            {...register("nid")}
-            id="outlined-basic"
-            label="NID"
-            type="number"
-            variant="outlined"
-            required
-          />
-          <TextField
-            {...register("birth")}
-            id="date"
-            label="Dath of Birth"
-            type="date"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            required
-          />
-          <TextField
-            {...register("department")}
-            id="outlined-select-currency"
-            select
-            label="Department"
-            value={department}
-            onChange={handleChange}
-            helperText="Please select your Department"
-            required
-          >
-            {departments.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            {...register("designation")}
-            id="outlined-basic"
-            label="Designation"
-            type="text"
-            variant="outlined"
-            required
-          />
+                    <Box className={dFlex}>
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("birth")}
+                            id="date"
+                            label="Dath of Birth"
+                            type="date"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            required
+                        />
+                        <TextField
+                            className={middleField}
+                            sx={{ width: "100%" }}
+                            {...register("department")}
+                            id="outlined-select-currency"
+                            select
+                            label="Department"
+                            value={department}
+                            onChange={handleChange}
+                            required
+                        >
+                            {departments.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
 
-          <Typography sx={{ m: 2 }} variant="h5">
-            Exprience
-          </Typography>
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("designation")}
+                            id="outlined-basic"
+                            label="Designation"
+                            type="text"
+                            variant="outlined"
+                            required
+                        />
+                    </Box>
 
-          <TextField
-            {...register("lastCompany")}
-            id="outlined-basic"
-            label="Company"
-            type="text"
-            variant="outlined"
-          />
-          <TextField
-            {...register("lastDepartment")}
-            id="outlined-basic"
-            label="Department"
-            type="text"
-            variant="outlined"
-          />
-          <TextField
-            {...register("lastDesignation")}
-            id="outlined-basic"
-            label="Designation"
-            type="text"
-            variant="outlined"
-          />
+                    {/* Experience Information */}
+                    <Typography sx={{ m: 2 }} variant="h5">
+                        Exprience
+                    </Typography>
 
-          <Typography sx={{ m: 2 }} variant="h5">
-            Education information
-          </Typography>
+                    <Box className={dFlex}>
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("lastCompany")}
+                            id="outlined-basic"
+                            label="Company"
+                            type="text"
+                            variant="outlined"
+                        />
+                        <TextField
+                            className={middleField}
+                            sx={{ width: "100%" }}
+                            {...register("lastDepartment")}
+                            id="outlined-basic"
+                            label="Department"
+                            type="text"
+                            variant="outlined"
+                        />
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("lastDesignation")}
+                            id="outlined-basic"
+                            label="Designation"
+                            type="text"
+                            variant="outlined"
+                        />
+                    </Box>
 
-          <TextField
-            {...register("lastDegree")}
-            id="outlined-basic"
-            label="Degree"
-            type="text"
-            variant="outlined"
-          />
-          <TextField
-            {...register("lastSubject")}
-            id="outlined-basic"
-            label="Subject"
-            type="text"
-            variant="outlined"
-          />
-          <TextField
-            {...register("lastInstitute")}
-            id="outlined-basic"
-            label="Institute"
-            type="text"
-            variant="outlined"
-          />
-          <TextField
-            {...register("lastGrade")}
-            id="outlined-basic"
-            label="Grade"
-            type="text"
-            variant="outlined"
-          />
-          <Button
-            variant="outlined"
-            style={{
-              backgroundColor: "#01578A",
-              color: "white",
-              marginTop: "1rem",
-            }}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Box>
-      </FormGroup>
-    </>
-  );
+                    {/* Education Information */}
+                    <Typography sx={{ m: 2 }} variant="h5">
+                        Education information
+                    </Typography>
+
+                    <Box className={dFlex}>
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("lastDegree")}
+                            id="outlined-basic"
+                            label="Degree"
+                            type="text"
+                            variant="outlined"
+                        />
+                        <TextField
+                            sx={{ width: "100%", marginLeft: "10px" }}
+                            {...register("lastSubject")}
+                            id="outlined-basic"
+                            label="Subject"
+                            type="text"
+                            variant="outlined"
+                        />
+                    </Box>
+
+                    <Box className={dFlex}>
+                        <TextField
+                            sx={{ width: "100%" }}
+                            {...register("lastInstitute")}
+                            id="outlined-basic"
+                            label="Institute"
+                            type="text"
+                            variant="outlined"
+                        />
+                        <TextField
+                            sx={{ width: "100%", marginLeft: "10px" }}
+                            {...register("lastGrade")}
+                            id="outlined-basic"
+                            label="Grade"
+                            type="text"
+                            variant="outlined"
+                        />
+                    </Box>
+                    <Button
+                        variant="outlined"
+                        style={{
+                            backgroundColor: "#01578A",
+                            color: "white",
+                            marginTop: "1rem ",
+                            marginBottom: "2rem ",
+                            padding: "12px 0px",
+                        }}
+                        type="submit"
+                    >
+                        Submit
+                    </Button>
+                </FormGroup>
+            </Box>
+        </Container>
+    );
 };
 
 export default MyInfo;
