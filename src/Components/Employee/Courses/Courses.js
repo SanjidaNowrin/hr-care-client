14bba8520ed936787003c0b7f035f38798760bec
foridislam332 <foridislam332>
2022-03-01 17:54:08 +0600

    course update

--- src/Components/Employee/Courses/Courses.js
+++ src/Components/Employee/Courses/Courses.js
@@ -1,6 +1,12 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Breadcrumbs, Container, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Course from "./Course/Course";
+
+// Breadcrumbs
+import Chip from '@mui/material/Chip';
+import { emphasize, styled } from '@mui/material/styles';
+import HomeIcon from '@mui/icons-material/Home';
+import { Link } from "react-router-dom";
 
 const Courses = () => {
     const [courses, setCourses] = useState([]);
@@ -8,16 +14,48 @@ const Courses = () => {
         fetch("https://ancient-thicket-61342.herokuapp.com/courses")
             .then((res) => res.json())
             .then((data) => setCourses(data?.data));
-    }, []);
+    }, [courses]);
+
+    // Breadcrumbs
+    const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+        const backgroundColor =
+            theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[800];
+        return {
+            backgroundColor,
+            height: theme.spacing(3),
+            color: theme.palette.text.primary,
+            fontWeight: theme.typography.fontWeightRegular,
+            '&:hover, &:focus': {
+                backgroundColor: emphasize(backgroundColor, 0.06),
+            },
+            '&:active': {
+                boxShadow: theme.shadows[1],
+                backgroundColor: emphasize(backgroundColor, 0.12),
+            },
+        };
+    });
 
     return (
         <Container>
-            <Typography
-                style={{ textAlign: "center", fontWeight: "500", marginTop: "1.5rem", marginBottom: "2rem" }}
-                variant="h4"
-            >
-                Our Popular <span style={{ color: " #01578A" }}>Courses</span>
-            </Typography>
+            {/* Breadcrumbs */}
+            <Box sx={{ mb: 4 }}>
+                <Typography
+                    sx={{ mt: 2, color: 'var(--p_color)' }} variant="h4">
+                    Skill Improvement Courses
+                </Typography>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link to="/dashboard">
+                        <StyledBreadcrumb
+                            to="/dashboard"
+                            label="Dashboard"
+                            icon={<HomeIcon fontSize="small" />}
+                        />
+                    </Link>
+                    <Link to="/dashboard/course"><StyledBreadcrumb component="a" href="#" label="Courses" /></Link>
+                </Breadcrumbs>
+            </Box>
             <Box sx={{ mb: 5 }}>
                 <Grid container spacing={4}>
                     {courses?.map((item) => (
