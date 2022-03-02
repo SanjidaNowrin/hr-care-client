faeff178b14b40f56154e8c49ddc33735d14f9ca
foridislam332 <foridislam332>
2022-03-02 11:48:26 +0600

    id card update

--- src/Components/Admin/IdCard/IdCard.js
+++ src/Components/Admin/IdCard/IdCard.js
@@ -3,11 +3,19 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import "./IdCard.css";
 import SingleId from "../SingleId/SingleId";
-import { Typography } from "@mui/material";
+import { Breadcrumbs, Container, TextField, Typography } from "@mui/material";
+
+// Breadcrumbs
+import Chip from '@mui/material/Chip';
+import { emphasize, styled } from '@mui/material/styles';
+import HomeIcon from '@mui/icons-material/Home';
+import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
 
 const IdCard = () => {
     const [employeesId, setemployeeId] = useState([]);
-    const [displayIdCards, setDisplayIdCards] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [filterData, setFilterData] = useState([]);
     // console.log(displayIdCards);
 
     useEffect(() => {
@@ -16,35 +24,99 @@ const IdCard = () => {
             .then((data) => setemployeeId(data.data));
     }, []);
 
-    //handle on change input
-    const handleOnChange = (event) => {
-        const searchText = event.target.value;
-        const matchIdCards = employeesId.filter((employeeId) => employeeId.name.toLowerCase().includes(searchText.toLowerCase()));
-        setDisplayIdCards(matchIdCards);
-        console.log(matchIdCards.length);
-    };
+    const handleOnChange = (e) => {
+        setInputValue(e.target.value)
+        console.log(e.target.value)
+    }
+
+    // filter employees id
+    useEffect(() => {
+        const filterID = employeesId.filter(data => data.ID == inputValue);
+        if (filterID.length > 0 || inputValue !== "") {
+            setFilterData(filterID)
+        } else {
+            setFilterData(employeesId)
+        }
+    }, [inputValue])
+
+    // click to search
+    const handleOnSearch = (e) => {
+        const filterID = employeesId.filter(data => data.ID == inputValue);
+        if (filterID.length > 0 || inputValue !== "") {
+            setFilterData(filterID)
+        } else {
+            setFilterData(employeesId)
+        }
+    }
+
+    // Breadcrumbs
+    const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+        const backgroundColor =
+            theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[800];
+        return {
+            backgroundColor,
+            height: theme.spacing(3),
+            color: theme.palette.text.primary,
+            fontWeight: theme.typography.fontWeightRegular,
+            '&:hover, &:focus': {
+                backgroundColor: emphasize(backgroundColor, 0.06),
+            },
+            '&:active': {
+                boxShadow: theme.shadows[1],
+                backgroundColor: emphasize(backgroundColor, 0.12),
+            },
+        };
+    });
 
     return (
-        <div>
-            <div className="id-card-area">
-                <div className="id-content">
-                    <Typography variant="h4" sx={{ fontWeight: "500", color: "#01578A", textAlign: "center", marginBottom: "18px" }}>
-                        Employee <span style={{ color: "#000" }}>ID Card</span>
-                    </Typography>
-                </div>
-                <div className="search-container">
-                    <input onChange={handleOnChange} type="text" placeholder="Search ID Card" />
-                    <Button className="btn_regular" sx={{ marginLeft: "1rem" }}>
-                        Search
-                    </Button>
-                </div>
-                <Grid container spacing={6}>
-                    {employeesId.map((employeeId) => (
-                        <SingleId key={employeeId._id} employeeId={employeeId}></SingleId>
-                    ))}
-                </Grid>
-            </div>
-        </div>
+        <Container>
+            {/* Breadcrumbs */}
+            <Box sx={{ mb: 4 }}>
+                <Typography
+                    sx={{ mt: 2, color: 'var(--p_color)' }} variant="h4">
+                    Employee ID Card
+                </Typography>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link to="/dashboard">
+                        <StyledBreadcrumb
+                            to="/dashboard"
+                            label="Dashboard"
+                            icon={<HomeIcon fontSize="small" />}
+                        />
+                    </Link>
+                    <Link to="/dashboard/id_card"><StyledBreadcrumb component="a" href="#" label="ID Card" /></Link>
+                </Breadcrumbs>
+            </Box>
+
+            {/* search box */}
+            <Box sx={{ textAlign: 'center', width: { xs: '80%', sm: '90%', md: '50%' }, margin: '0 auto', position: 'relative', mb: 6 }}
+                className="id_search"
+            >
+                <TextField
+                    placeholder="Search ID Card"
+                    variant="outlined"
+                    onChange={handleOnChange}
+                    sx={{ width: '100%' }}
+                />
+
+                <Button
+                    sx={{ position: 'absolute', top: '50%', right: '-45px', transform: 'translate(-50%, -50%)' }}
+                    className="btn_regular"
+                    onClick={handleOnSearch}
+                >
+                    Search
+                </Button>
+            </Box>
+
+            {/* card box */}
+            <Grid container spacing={4} sx={{ mb: 4 }}>
+                {filterData.slice(0, 3).map((employeeId) => (
+                    <SingleId key={employeeId._id} employeeId={employeeId}></SingleId>
+                ))}
+            </Grid>
+        </Container>
     );
 };
 
--- src/Components/Admin/SingleId/SingleId.js
+++ src/Components/Admin/SingleId/SingleId.js
@@ -1,85 +1,166 @@
 import { useRef } from "react";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import "./SingleId.css";
 import Swal from 'sweetalert2'
 import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
+import { makeStyles } from "@mui/styles";
+import { Box, Divider, Paper, Typography } from "@mui/material";
+import useAuth from "../../../hooks/useAuth";
+
+
 const SingleId = ({ employeeId }) => {
-  const { name, department, designation, phone } = employeeId;
-  const pdfExportComponent = useRef(null);
-  const handleOnclick = () => {
-    pdfExportComponent.current.save();
+    const { user } = useAuth();
+    const { ID, name, department, designation, phone } = employeeId;
 
-    Swal.fire(
-      'Good job!',
-      'ID Card Downloaded Successfully!',
-      'success'
-    )
+    const pdfExportComponent = useRef(null);
+    const handleOnclick = () => {
+        pdfExportComponent.current.save();
 
-  };
-  return (
+        Swal.fire(
+            'Good job!',
+            'ID Card Downloaded Successfully!',
+            'success'
+        )
 
-    <Grid item xs={12} sm={12} md={4}>
-      <PDFExport ref={pdfExportComponent}>
-        <div className="card" id="idCards">
-          <div className="upper-content">
-            <h4 className="upper-title">Hr Care</h4>
-            <div className="img-container">
-              <img
-                src="https://i.ibb.co/DGPYn0C/person.jpg"
-                alt=""
-                height="100px"
-                width="100px"
-              />
-            </div>
-          </div>
-          <div className="lower-content">
-            <h4>{name}</h4>
-            <p style={{ fontSize: "14px" }}>{department}</p>
-          </div>
-          <div className="bottom-content-id">
-            <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <h5>ID NO</h5>
-                <p>S-15012</p>
-                <h5>D.O.B</h5>
-                <p>22/4/2021</p>
-              </Grid>
-              <Grid item xs={4}>
-                <h5>Joined Date</h5>
-                <p>22/06/2021</p>
-                <h5>Designation</h5>
-                <p>{designation}</p>
-              </Grid>
-              <Grid item xs={4}>
-                <h5>Phone</h5>
-                <p>{phone}</p>
-                <h5>B Group</h5>
-                <p>B+</p>
-              </Grid>
-            </Grid>
-          </div>
-          <div className="qr-area">
-            <h4 className="qr">QR Code</h4>
-            <img
-              width="100%"
-              src="https://i.ibb.co/fxLKH5h/qr-code-g151cf237e-1280.png"
-              alt=""
-            />
-          </div>
-        </div>
-      </PDFExport>
-      <div className="form-button">
-        <Button
-          className="btn_regular"
-          onClick={() => handleOnclick()}
-          variant="contained"
-        >
-          Download PDF
-        </Button>
-      </div>
-    </Grid>
+    };
 
-  );
+    const useStyle = makeStyles({
+        cardBox: {
+            overflow: 'hidden',
+            transition: 'all .3s ease',
+            "&:hover": {
+                boxShadow: '1px 10px 30px #b6b7b7 !important'
+            },
+        },
+        cardTop: {
+            background: 'var(--p_color)',
+            height: '110px',
+            paddingTop: '10px'
+        },
+        imgBox: {
+            textAlign: 'center',
+            margin: '0 auto',
+            borderRadius: '50%',
+            position: 'relative',
+            top: '-53px',
+            height: '110px',
+            width: '110px',
+        },
+        imgTop: {
+            width: '100%',
+            borderRadius: '50%',
+            border: '6px solid #fff',
+        },
+        textBold: {
+            fontWeight: '700 !important'
+        },
+        signatureImg: {
+            width: '60px'
+        }
+    })
+
+    const { cardBox, cardTop, imgBox, imgTop, textBold, signatureImg } = useStyle();
+
+    return (
+        <Grid item xs={12} sm={12} md={4}>
+            <PDFExport ref={pdfExportComponent}>
+                <Paper className={cardBox}>
+                    <Box className={cardTop}>
+                        <Typography sx={{ textAlign: 'center', color: '#fff', textTransform: 'uppercase' }} variant="h4">HR Care</Typography>
+                    </Box>
+
+                    {/* employee image */}
+                    <Box className={imgBox}>
+                        <img
+                            src={user.photoURL}
+                            alt=""
+                            className={imgTop}
+                        />
+                    </Box>
+
+                    <Box sx={{ textAlign: 'center', marginTop: '-53px' }}>
+                        <Typography sx={{ textShadow: '2px 2px 9px #969696' }} variant="h5">{name}</Typography>
+                        <Typography sx={{ color: '#845EC2', textShadow: '2px 2px 9px #969696' }} variant="body1">{department}</Typography>
+                    </Box>
+
+                    {/* body content */}
+                    <Box sx={{ px: 2 }}>
+                        <Grid sx={{ mt: 1 }} container spacing={1}>
+                            <Grid item xs={4}>
+                                <Typography className={textBold} variant="body2">ID NO</Typography>
+                                <Typography variant="body2">{name.slice(0, 1)}#{ID}</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography className={textBold} variant="body2">Phone</Typography>
+                                <Typography variant="body2">{phone}</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography className={textBold} variant="body2">Joined Date</Typography>
+                                <Typography variant="body2">22/06/2021</Typography>
+                            </Grid>
+                        </Grid>
+
+                        <Grid sx={{ marginTop: '5px' }} container spacing={1}>
+                            <Grid item xs={4}>
+                                <Typography className={textBold} variant="body2">D.O.B</Typography>
+                                <Typography variant="body2">22/4/2021</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography className={textBold} variant="body2">Designation</Typography>
+                                <Typography variant="body2">{designation}</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography className={textBold} variant="body2">Blood Group</Typography>
+                                <Typography variant="body2">B+</Typography>
+                            </Grid>
+                        </Grid>
+                    </Box>
+
+                    <Divider sx={{ my: 2 }} variant="middle" />
+
+                    {/* signature box */}
+                    <Box sx={{ px: 2, pt: 1, pb: 2 }}>
+                        <Grid container spacing={10}>
+                            <Grid item xs={6}>
+                                <Box sx={{ textAlign: 'center' }}>
+                                    <img
+                                        className={signatureImg}
+                                        src="https://signaturely.com/wp-content/uploads/2020/04/mark-cuban-signature-signaturely-image.png"
+                                        alt="signature"
+                                    />
+                                </Box>
+
+                                <Divider />
+                                <Typography sx={{ textAlign: 'center' }} variant="body1">Employee</Typography>
+                            </Grid>
+
+                            <Grid item xs={6}>
+                                <Box sx={{ textAlign: 'center' }}>
+                                    <img
+                                        className={signatureImg}
+                                        src="https://www.seoclerk.com/pics/211935-1qeWBB1398605222.jpg"
+                                        alt="signature"
+                                    />
+                                </Box>
+                                <Divider />
+                                <Typography sx={{ textAlign: 'center' }} variant="body1">Director</Typography>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Paper>
+            </PDFExport>
+            <Box sx={{ textAlign: 'center', my: 1 }}>
+                <Button
+                    className="btn_regular"
+                    onClick={() => handleOnclick()}
+                    variant="contained"
+                    sx={{ padding: '6px 50px 5px !important' }}
+                >
+                    Download PDF
+                </Button>
+            </Box>
+        </Grid >
+
+    );
 };
 export default SingleId;
