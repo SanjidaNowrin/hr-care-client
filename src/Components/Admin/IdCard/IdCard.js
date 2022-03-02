7bae068bdc4d1fa24b999b6081b06895725c883c
foridislam332 <foridislam332>
2022-03-02 12:12:42 +0600

    Id search update

--- src/Components/Admin/IdCard/IdCard.js
+++ src/Components/Admin/IdCard/IdCard.js
@@ -16,7 +16,6 @@ const IdCard = () => {
     const [employeesId, setemployeeId] = useState([]);
     const [inputValue, setInputValue] = useState('');
     const [filterData, setFilterData] = useState([]);
-    // console.log(displayIdCards);
 
     useEffect(() => {
         fetch("https://ancient-thicket-61342.herokuapp.com/employees")
@@ -25,24 +24,24 @@ const IdCard = () => {
     }, []);
 
     const handleOnChange = (e) => {
-        setInputValue(e.target.value)
-        console.log(e.target.value)
+        const number = parseInt(e.target.value)
+        setInputValue(number)
     }
 
     // filter employees id
     useEffect(() => {
-        const filterID = employeesId.filter(data => data.ID == inputValue);
-        if (filterID.length > 0 || inputValue !== "") {
+        const filterID = employeesId.filter(data => parseInt(data.ID) === inputValue);
+        if (filterID.length > 0) {
             setFilterData(filterID)
         } else {
             setFilterData(employeesId)
         }
-    }, [inputValue])
+    }, [inputValue, employeesId])
 
     // click to search
     const handleOnSearch = (e) => {
-        const filterID = employeesId.filter(data => data.ID == inputValue);
-        if (filterID.length > 0 || inputValue !== "") {
+        const filterID = employeesId.filter(data => parseInt(data.ID) === inputValue);
+        if (filterID.length > 0) {
             setFilterData(filterID)
         } else {
             setFilterData(employeesId)
@@ -121,60 +120,3 @@ const IdCard = () => {
 };
 
 export default IdCard;
-
-/*
-
-
-<div className="employee-id">
-                    <div className="employee-id-details">
-                        <div className="top-content-id">
-                            <h3>Hr <span style={{color: 'salmon'}}>Care</span></h3>
-                            <p>Human Resourse</p>
-                        </div>
-                        <div className="middle-content-id">
-                            <Grid container spacing={2}>
-                                <Grid item xs={4}>
-                                    <div className="img-id">
-                                    <img src="https://i.ibb.co/DGPYn0C/person.jpg" alt="" />
-                                    </div>
-                                </Grid>
-                                <Grid item xs={8}>
-                                    <h3>Mohammad Forid</h3>
-                                    <p>Creative Manager</p>
-                                </Grid>
-                            </Grid>
-                        </div>
-                        <div className="bottom-content-id">
-                            <Grid container spacing={2}>
-                                <Grid item xs={3}>
-                                    <h3>ID NO</h3>
-                                    <p>S-15012</p>
-                                    <h3>D.O.B</h3>
-                                    <p>22/4/2021</p>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <h3>Joined Date</h3>
-                                    <p>22/06/2021</p>
-                                    <h3>Expire Date</h3>
-                                    <p>22/3/2025</p>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <h3>Phone</h3>
-                                    <p>01855555</p>
-                                    <h3>B Group</h3>
-                                    <p>B+</p>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <h3 className="qr">QR Code</h3>
-                                    <img src="https://i.ibb.co/fxLKH5h/qr-code-g151cf237e-1280.png" alt="" />
-                                </Grid>
-                            </Grid>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="form-button">
-                    <Button variant="contained">Download PDF</Button>
-                    </div>
-*/
