9d8be372227edf258e5567de250dad1c6ef7f8d2
asrafulsow <aisowrav11@gmail.com>
2022-03-04 12:11:51 +0600

    homepageUpdate

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,66 +1,42 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import { Typography, Button, Container, useTheme } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import { Link } from "react-router-dom";
-const Banner = () => {
-    const theme = useTheme();
-    const useStyle = makeStyles({
-        verticalCenter: {
-            display: "flex",
-            alignItems: "center",
-            [theme.breakpoints.down("sm")]: {
-                textAlign: 'center !important'
-            }
-        },
-        bannerTitle: {
-            marginBottom: '20px !important',
-            fontWeight: '700 !important',
-            color: '#01578A'
-        }
-    })
+import {  Container } from "@mui/material";
+import Carousel from 'react-material-ui-carousel';
+import Item from "../Item/Item";
 
-    const { verticalCenter, bannerTitle } = useStyle();
+const Banner = () => {
+
+    var items = [
+        {
+            name: " HR Care",
+            title: "Human Resource Management Admin Template",
+            description: "HR Software is system for company to maintain a database of theiremployers performance. Using this software, a company can manage their employer details, attendance, Leave, holidays, Salary, etc.",
+            img: "https://i.ibb.co/1q179X9/output-onlinepngtools-1.png"
+        },
+        {
+            name: "Our Design",
+            title: "Our Goal Is Employee Satisfication",
+            description: "HR Software is system for company to maintain a database of theiremployers performance. Using this software, a company can manage their employer details, attendance, Leave, holidays, Salary, etc.",
+            img: "https://i.ibb.co/XjzvjsN/output-onlinepngtools-2.png"
+        },
+        {
+            name: "Dashboard System",
+            title: "Our Main Focuse Is Dashboard",
+            description: "HR Software is system for company to maintain a database of theiremployers performance. Using this software, a company can manage their employer details, attendance, Leave, holidays, Salary, etc.",
+            img: "https://i.ibb.co/52VcGBf/banner.png"
+        },
+        
+    ]
+
 
     return (
-        <Box sx={{ py: 10 }}>
-            <Container>
-                <Grid container spacing={8}>
-                    <Grid
-                        item
-                        xs={12}
-                        md={6}
-                        className={verticalCenter}
-                    >
-                        <Box>
-                            <Typography className={bannerTitle} variant="h3">
-                                HR Care
-                            </Typography>
-                            <Typography variant="h5">
-                                Human Resource Management Admin Template
-                            </Typography>
-                            <Typography variant="body1" sx={{ mt: 1, color: '#818181', fontFamily: 'var(--PT_font)' }}>
-                                HR Software is system for company to maintain a database of their
-                                employers performance. Using this software, a company can manage their employer details, attendance, Leave, holidays, Salary, etc.
-                            </Typography>
-                            <Link to="/dashboard">
-                                <Button className="btn_regular" sx={{ mt: 3 }}>
-                                    Let's Start
-                                </Button>
-                            </Link>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <img
-                            src="https://i.ibb.co/52VcGBf/banner.png"
-                            alt="banner"
-                            border="0"
-                            style={{ width: "100%" }}
-                        />
-                    </Grid>
-                </Grid>
-            </Container>
+
+        <Box>
+            <Carousel>
+            {
+                items.map( (item, i) => <Item key={i} item={item} /> )
+            }
+        </Carousel>
         </Box>
     );
 };
