0c3198a4a4a0a7e23b8696253d5529965fd52eb5
NurulIslam-EEE <ni163034@gmail.com>
2022-03-11 20:48:42 +0600

    update Employee dashboard

--- src/Components/Dashboard/Charts/PieChartsChartJs.js
+++ src/Components/Dashboard/Charts/PieChartsChartJs.js
@@ -8,26 +8,31 @@ ChartJS.register(
 )
 
 
-const PieChartsChartJs = () => {
+const PieChartsChartJs = ({ attendance, dateString, holiday }) => {
+
+    const w = attendance.length
+    const h = holiday.length;
+    const p = w - h
+    const p = w - (ab + h)
     const data = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Present', 'Absent', 'Holiday'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [p, h, h],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
+
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
+
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
+
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
+
                 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
@@ -37,7 +42,16 @@ const PieChartsChartJs = () => {
         maintainAspectRatio: false,
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                grid: {
+                    display: false
+                }
+            },
+            x: {
+
+                grid: {
+                    display: false
+                }
             }
         },
         length: {
--- src/Components/Dashboard/DashboardHome/EmployeeDashboardHome.js
+++ src/Components/Dashboard/DashboardHome/EmployeeDashboardHome.js
@@ -7,35 +7,62 @@ import TrendingDownIcon from '@material-ui/icons/TrendingDown';
 import TodayIcon from '@material-ui/icons/Today';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import { makeStyles } from '@mui/styles';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import HomeIcon from '@mui/icons-material/Home';
+import { Link } from 'react-router-dom';
+import { emphasize, styled } from '@mui/material/styles';
+import Chip from '@mui/material/Chip';
 import PieCharts from '../Charts/PieCharts';
 import LineCharts from '../Charts/LineCharts';
 import BarChartJs from '../Charts/BarChartJs';
 import DoughnutChartJs from '../Charts/DoughnutChartJs';
 import LineChartsChartJs from '../Charts/LineChartsChartJs';
 import PieChartsChartJs from '../Charts/PieChartsChartJs';
-import dateFormat from '../../Share/Navbar/dateFormat';
+import dateFormat from '../../Share/DateFormat/dateFormat';
 import useAuth from '../../../hooks/useAuth';
 const EmployeeDashboardHome = () => {
     const { user } = useAuth();
     console.log(user)
     const [attendance, setAttendance] = useState([]);
+    const [holiday, setHoliday] = useState([]);
     // fetch attendence
 
     const currentDate = dateFormat(new Date().toLocaleString().split(",")[0], 'yyyy-MM-dd');
     const dateString = currentDate.split('-')[1];
     console.log(dateString, currentDate)
     useEffect(() => {
-        fetch('https://ancient-thicket-61342.herokuapp.com/attendance/nowrin.cse18@gmail.com')
+        fetch(`https://ancient-thicket-61342.herokuapp.com/attendance/${user.email}`)
             .then(res => res.json())
             .then(data => {
 
                 const filteredData = data?.result?.filter(x => x?.date?.split('-')[1] === dateString)
-                console.log(data.result, filteredData)
+
+                const filteredHoliday = data?.result?.filter(y => y.entry === '')
                 setAttendance(filteredData)
+                setHoliday(filteredHoliday)
+                console.log(data.result, filteredData, filteredHoliday)
             })
     }, [])
 
-
+    const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+        const backgroundColor =
+            theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[800];
+        return {
+            backgroundColor,
+            height: theme.spacing(3),
+            color: theme.palette.text.primary,
+            fontWeight: theme.typography.fontWeightRegular,
+            '&:hover, &:focus': {
+                backgroundColor: emphasize(backgroundColor, 0.06),
+            },
+            '&:active': {
+                boxShadow: theme.shadows[1],
+                backgroundColor: emphasize(backgroundColor, 0.12),
+            },
+        };
+    });
 
     const useStyle = makeStyles({
         dashTopBox: {
@@ -58,6 +85,18 @@ const EmployeeDashboardHome = () => {
     const { dashTopText, dashTopBox } = useStyle();
     return (
         <Container sx={{ background: '#F7F9FE' }}>
+            <Typography sx={{ mt: 2, color: 'var(--p_color)' }} variant="h4">
+                Dashboard
+            </Typography>
+            <Breadcrumbs aria-label="breadcrumb">
+                <Link to="/dashboard">
+                    <StyledBreadcrumb
+                        to="/dashboard"
+                        label="Dashboard"
+                        icon={<HomeIcon fontSize="small" />}
+                    />
+                </Link>
+            </Breadcrumbs>
 
             <Box sx={{ mt: 3 }}>
                 <Grid container spacing={3}>
@@ -68,7 +107,7 @@ const EmployeeDashboardHome = () => {
                                 <Typography variant="h5" sx={{ color: 'rgba(75, 192, 192, 1)' }}>
                                     Total Working Day
                                 </Typography>
-                                <TodayIcon></TodayIcon>
+                                {/* <TodayIcon></TodayIcon> */}
                                 <Typography variant="h4" sx={{ textAlign: 'center', color: 'rgba(75, 192, 192, 1)', py: 1 }}>
                                     {attendance.length}
                                 </Typography>
@@ -87,7 +126,7 @@ const EmployeeDashboardHome = () => {
                                 <Typography variant="h4" sx={{ textAlign: 'center', color: 'rgba(255, 206, 86, 1)', py: 1 }}>
                                     {attendance.length}
                                 </Typography>
-                                <TrendingUpIcon></TrendingUpIcon>
+                                {/* <TrendingUpIcon></TrendingUpIcon> */}
                             </Box>
                         </Box>
                     </Grid>
@@ -103,7 +142,7 @@ const EmployeeDashboardHome = () => {
                                 <Typography variant="h6" sx={{ textAlign: 'center', color: 'rgba(255, 99, 132, .8)', py: 1 }}>
                                     0
                                 </Typography>
-                                <TrendingDownIcon></TrendingDownIcon>
+                                {/* <TrendingDownIcon></TrendingDownIcon> */}
                             </Box>
                         </Box>
                     </Grid>
@@ -133,18 +172,23 @@ const EmployeeDashboardHome = () => {
                 <Box sx={{ height: '300px', width: '50%', margin: '20px', background: 'white' }}>
                     <BarChartJs></BarChartJs>
                 </Box>
-                <Box sx={{ height: '300px', width: '50%', margin: '20px', background: 'white' }}>
+                {/* <Box sx={{ height: '300px', width: '50%', margin: '20px', background: 'white' }}>
                     <DoughnutChartJs attendance={attendance} dateString={dateString}></DoughnutChartJs>
+                </Box> */}
+                <Box sx={{ height: '300px', width: '50%', margin: '30px', background: 'white' }}>
+                    <Typography variant="h6" sx={{ color: 'rgba(255, 159, 64, 1)' }}>
+                        Attendance
+                    </Typography>
+
+                    <PieChartsChartJs holiday={holiday} attendance={attendance} dateString={dateString}></PieChartsChartJs>
                 </Box>
             </Box>
 
             <Box sx={{ height: '300px', width: '100%', display: 'flex', marginTop: '40px' }}>
-                <Box sx={{ height: '300px', width: '50%', margin: '30px', background: 'white' }}>
+                <Box sx={{ height: '300px', width: '100%', margin: '30px', background: 'white' }}>
                     <LineChartsChartJs></LineChartsChartJs>
                 </Box>
-                <Box sx={{ height: '300px', width: '50%', margin: '30px', background: 'white' }}>
-                    <PieChartsChartJs></PieChartsChartJs>
-                </Box>
+
             </Box>
 
             {/* 
