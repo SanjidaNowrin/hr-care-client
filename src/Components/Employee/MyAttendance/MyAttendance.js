2738c717c47474f1d3b3b60db626c4b72b4167bc
md-mh <mmahedi49@gmail.com>
2022-03-13 19:12:00 +0600

    update attendance status

--- src/Components/Employee/MyAttendance/MyAttendance.js
+++ src/Components/Employee/MyAttendance/MyAttendance.js
@@ -69,6 +69,7 @@ const MyAttendance = () => {
     entryTime.date = dateFormat(time.split(",")[0], "yyyy-MM-dd");
     entryTime.entry = time.split(",")[1];
     entryTime.leave = "";
+    entryTime.status = "Present";
 
     if (today?.date === entryTime.date) {
       Swal.fire("You already Punched In");
